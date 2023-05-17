4e3342ad5378b9b4036f0e2271f84a25ef0a710b
ferazzzq <133917887+ferazzzq@users.noreply.github.com>
2023-05-17 17:56:16 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
 const { AoiClient, LoadCommands } = require("aoi.js");
-
 const aoijs = require("aoi.js")
 
 const bot = new AoiClient({
 
     token: "seu token aqui",
-
     prefix: "$getGuildVar[prefixo]",
-
     intents: ["MessageContent", "Guilds", "GuildMessages", "GuildVoiceStates"],
-
     events: ["onMessage", "onInteractionCreate"],
 
     database: {
 
         type: "aoi.db",
-
         db: require("aoi.db"),
-
         tables: ["main"],
-
         path: "./database/",
-
         extraOptions: {
-
             dbType: "KeyValue"
 
         }
@@ -33,7 +24,6 @@ const bot = new AoiClient({
 });
 
 const loader = new LoadCommands(bot);
-
 loader.load(bot.cmd, "./comandos/")
 
 bot.variables(require("./variaveis/principal.js"));
@@ -43,23 +33,16 @@ bot.readyCommand({
       channel:"id do canal de logs on",
 
       code:`
-
 to on ze buceta
-
 `
 
 })
 
 bot.status({
-
   text: "EstelarBot - Desenvolvimento.", 
-
   type: "STREAMING", 
-
   status: "online",
-
   URL: "https://youtube.com/@ferrazzzq",
-
   time: 12 
 
 });
