982e483af0bab1b968fac8698b27d30de43c0f6d
ferazzzq <133917887+ferazzzq@users.noreply.github.com>
2023-12-10 12:22:02 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -1,47 +1,51 @@
-const { AoiClient, LoadCommands } = require("aoi.js");
-const aoijs = require("aoi.js")
+const express = require('express')
+const app = express();
+app.get("/", (request, response) => {
+  const ping = new Date();
+  ping.setHours(ping.getHours() - 3);
+  console.log(`Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`);
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
 
-const bot = new AoiClient({
-
-    token: "seu token aqui",
-    prefix: "$getGuildVar[prefixo]",
-    intents: ["MessageContent", "Guilds", "GuildMessages", "GuildVoiceStates"],
-    events: ["onMessage", "onInteractionCreate"],
-
-    database: {
-
-        type: "aoi.db",
-        db: require("aoi.db"),
-        tables: ["main"],
-        path: "./database/",
-        extraOptions: {
-            dbType: "KeyValue"
-
-        }
-
-    }
-
+const { AoiClient, LoadCommands, Util } = require("aoi.js");
+const { parse, createAst } = require('@akarui/aoi.parser');
+const { parseExtraOptions } = require('@akarui/aoi.parser/components');
+const client = new AoiClient({
+  token: "SEU_ToKeN",
+  prefix: "SEU_ToKeN",
+  intents: ["MessageContent", "Guilds", "GuildMessages"],
+  events: ["onMessage", "onInteractionCreate"],
+  disableAoiDB: true,
+  database: {
+    type: "aoi.db",
+      db: require("@akarui/aoi.db"),
+      dbType: "KeyValue",
+      tables: ["main"],
+      securityKey: "uma key com 32 caracteres.",
+  },
 });
 
-const loader = new LoadCommands(bot);
-loader.load(bot.cmd, "./comandos/")
-
-bot.variables(require("./variaveis/principal.js"));
-
-bot.readyCommand({
-
-      channel:"id do canal de logs on",
-      code:`
-to on ze buceta
-`
-
-})
-
-bot.status({
-  text: "EstelarBot - Desenvolvimento.", 
-  type: "STREAMING", 
-  status: "online",
-  URL: "https://youtube.com/@ferrazzzq",
-  time: 12 
-
+process.on('unhandRejection', (reason, promise) => {
+  console.log(reason, promise)
 });
+
+process.on('uncaughtException', (error, origin) => {
+   console.log(error, origin)
+});
+
+client.status({
+  name: "bot focused on global economy.",
+  type: "PLAYING",
+  time: 12,
+});
+
+Util.parsers.ErrorHandler = parse;
+Util.parsers.OptionsParser = (data) => {
+  return createAst(data).children.map(parseExtraOptions);
+}
+
+client.variables(require("./variaveis/principal.js"));
+
+const loader = new LoadCommands(client);
+loader.load(client.cmd, "./comandos/")
